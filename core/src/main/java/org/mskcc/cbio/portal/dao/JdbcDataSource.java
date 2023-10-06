e1b7f40c69c6d38d64d66a6b8eba4e4ac2dcf36a
Avery Wang <averyjwang@gmail.com>
2023-10-06 14:20:14 -0700

    update warning

--- core/src/main/java/org/mskcc/cbio/portal/dao/JdbcDataSource.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/JdbcDataSource.java
@@ -4,11 +4,16 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.mskcc.cbio.portal.util.DatabaseProperties;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 /**
  * Data source that self-initializes based on cBioPortal configuration.
  */
 public class JdbcDataSource extends BasicDataSource {
+    private static final Logger LOG = LoggerFactory.getLogger(JdbcDataSource.class);
 
     public JdbcDataSource () {
         DatabaseProperties dbProperties = DatabaseProperties.getInstance();
@@ -20,6 +25,19 @@ public class JdbcDataSource extends BasicDataSource {
         String database = dbProperties.getDbName();
         String enablePooling = (!StringUtils.isBlank(dbProperties.getDbEnablePooling())) ? dbProperties.getDbEnablePooling(): "false";
         String connectionURL = dbProperties.getConnectionURL();
+
+	String message = "\n----------------------------------------------------------------------------------------------------------------" +
+                "-- Connection error:\n" +
+                "-- You try to connect to the database using the deprecated 'db.host', 'db.portal_db_name' and 'db.use_ssl' properties.\n" +
+                "-- Please remove these properties and use the 'db.connection_string' property instead. See https://docs.cbioportal.org/deployment/customization/portal.properties-reference/\n" +
+                "-- for assistance on building a valid connection string.\n" +
+                "----------------------------------------------------------------------------------------------------------------\n";
+        if (defined(host) || defined(database) || defined(dbProperties.getDbUseSSL())) {
+        	LOG.info(message);
+		System.err.println(message);
+		System.out.println(message);
+	}
+
 
         Assert.hasText(userName, errorMessage("username", "db.user"));
         Assert.hasText(password, errorMessage("password", "db.password"));
